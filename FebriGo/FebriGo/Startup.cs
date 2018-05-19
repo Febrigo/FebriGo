605b80c028f88f363f429c4ffb19065cf01f4778
TawhidurTanim <salazarslytherin101@gmail.com>
2018-05-20 01:06:27 +0600

    FileStructure

---
+++ FebriGo/FebriGo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FebriGo.Startup))]
+namespace FebriGo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
